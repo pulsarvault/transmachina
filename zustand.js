5c061564304a69d6989fb2661cf5749c9b6e6a3b
NEUTRON STAR <pulsarsafe@gmail.com>
2026-06-18 00:26:32 +0530

    Zustand library Creation for Samir: Day 2

--- zustand.js
+++ zustand.js
@@ -1,4 +1,4 @@
-// Zustand like Library creation (Learning: Day 1 for Samir)
+// Zustand like Library creation (Learning: Day 2 for Samir)
 
 // Zustand-lite library Code
 const createStore = (initializer) => {
@@ -25,30 +25,10 @@ const result = initializer(setState, getState); // This is the key moment where 
 
   return { // keep return and { on same line or JS will do automatic semicolon insertion
     getState,
-    setState
+    setState,
+    ...result
   };
 
 };
 
-// Zustand-lite Usage: Call createStore with an arrow function as an argument. myStore Closure object is created.
-// myStore = {
-//  getState: () => state,     // Function to read state
-//  setState: (partial) => {}  // Function to update state
-// }
-// The state (health, score) is HIDDEN inside the closure!
-// You can only access it through getState() and setState()
-// 'set' and 'get' are REFERENCES (NOT called yet) 
-// initializer in createStore(initializer) then holds reference to the (set, get) arrow function.  
-// set and get are just useful shorthands
- 
-const myStore = createStore((set, get) => {
-return {
-	health: 100,
-	score: 0
-};
-});
-
-// get() calls getState and set calls setState in the library
-console.log('State: ', myStore.getState());
-myStore.setState({score: 82});
-console.log('New State: ', myStore.getState());
+// Zustand-lite Usage: Call createStore with an arrow function 
